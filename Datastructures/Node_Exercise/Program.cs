7a36f6cfaf90219df373448c8f8b47c019573497
haUk2 <91018531+haUk2@users.noreply.github.com>
2022-12-02 10:01:41 +0100

    initialize

---
+++ Datastructures/Node_Exercise/Program.cs
@@ -0,0 +1,31 @@
+﻿using Datastructures;
+using System.Data.Common;
+namespace GenerischeKlassen
+{
+    class Program
+    {
+        public static void Main(String[] args)
+        {
+            Datastructures.SingleLinkedList linkedList = new Datastructures.SingleLinkedList();
+            linkedList.insertLast(1);
+            linkedList.insertLast(4);
+            linkedList.insertLast(4);
+            linkedList.insertLast(6);
+            linkedList.insertLast(5);
+            //linkedList.InsertAfter(9, 2);
+            //linkedList.insert_First(7);
+            //linkedList.DeleteNode(3);
+            //linkedList.GetNode(4);
+            //linkedList.SwitchNode(2, 4);
+            //linkedList.SwitchNode2(new Node(2, null), new Node(4, null));
+            Console.WriteLine("Linked List before Sorting..");
+            linkedList.printList();
+            //Console.WriteLine("\nLinkedList After sorting");
+            //ISort.insertionSort(linkedList.head);
+            Console.WriteLine("\nLinkedList After sorting reverse");
+            //linkedList.insertionSortInverse(linkedList.head);
+            linkedList.printList();
+            //Console.WriteLine(linkedList.GetNode(4));
+        }
+    }
+}
---
+++ Datastructures/SingleLinkedListTests/LinkedListTest.cs
@@ -0,0 +1,66 @@
+﻿using Common;
+using Datastructures;
+using NUnit.Framework;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+
+namespace TestProject
+{
+    public class LinkedListTest
+    {
+        public Node head;
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void TestSLLCount_AddItems_ReturnCorrectCount()
+        {
+            var myLL = new SingleLinkedList();
+            myLL.insertLast(1);
+            myLL.insertLast(1);
+            myLL.insertLast(1);
+            myLL.insertLast(1);
+            Assert.AreEqual(4, myLL.count());
+        }
+
+        [Test]
+        public void SingleLinkedListTest_DeleteItem_ReturnCorrectCount()
+        {
+            var linkedList = new SingleLinkedList();
+            linkedList.insertLast(0);
+            //linkedList.InsertAfter(3, 0);
+            Assert.AreEqual(linkedList.count(), 2);
+            linkedList.DeleteNode(0);
+            Assert.AreEqual(linkedList.count(), 1);
+        }
+
+        [Test]
+        public void SSLTest_NodeExists_ReturnsNode()
+        {
+            var linkedList = new SingleLinkedList();
+            var getnode = linkedList.GetNode(1);
+            Assert.AreEqual(getnode, null);
+            linkedList.insertLast(1);
+            linkedList.insertLast(2);
+            linkedList.insertLast(3);
+            getnode = linkedList.GetNode(1);
+            Assert.AreNotEqual(getnode, null);
+        }
+
+        [Test]
+        public void SSLTest_SwitchNodes_ReturnCorrectOrder()
+        {
+            var linkedList = new SingleLinkedList();
+            linkedList.insertLast(1);
+            linkedList.insertLast(1);
+            linkedList.insertLast(5);
+            var node1 = linkedList.GetNode(1).data;
+            var node2 = linkedList.GetNode(5).data;
+            linkedList.SwitchNode(node1, node2);
+            Assert.AreEqual(5, node1);
+
+        }
+    }
+}
